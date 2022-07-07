56daaa7b415a79785c1b2e37f39df14eaebb5775
Linar <alr86@mail.ru>
2022-07-07 22:35:13 +0300

    Added task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿int Sum(int n)
+{
+    string b = Convert.ToString(n);
+    int result = 0;
+      for(int i = 0; i <= b.Length; i++)
+    {
+        int n1 = n % 10;
+        n /= 10;
+        result = result + n1;
+    }
+    return result;
+}
+
+Console.Write("Input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("The sum of number is: " + Sum(n));
